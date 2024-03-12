dd9d85db5f571142f81b3da4b2d07696842dbfd2
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 16:55:09 +0100

    feat: add the for cycle to consider every single figure

--- jssnack-bonus/script.js
+++ jssnack-bonus/script.js
@@ -1,8 +1,10 @@
 // Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
 
-const fourFiguresNumber = parseInt(prompt('Scrivi un numero di 4 cifre')); // Chiediamo un numero di 4 cifre 
-console.log(fourFiguresNumber);
+const fourFiguresNumber = prompt('Scrivi un numero di 4 cifre'); // Chiediamo un numero di 4 cifre 
 
+for (let i = 0; i < fourFiguresNumber.length; i++) {
+    console.log(fourFiguresNumber[i]);
 
+}
 // Prendiamo in considerazione le singole cifre
 // Sommiamo le cifre l'una con l'altra
