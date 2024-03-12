3a47f011b8d656b6dc6aa7f31409c36bf972f39b
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 14:33:23 +0100

    feat: add verify that the console can count the amount of letter of every word written in the prompt

--- js/jssnack-2/script.js
+++ js/jssnack-2/script.js
@@ -3,6 +3,11 @@
 
 const firstWord = prompt('Scrivi una parola'); // Chiediamo due parole con due prompt
 const secondWord = prompt("Scrivi un'altra parola");
+
+console.log(firstWord.length);
+console.log(secondWord.length);
+
+
 // Verifichiamo se una è più corta dell'altra
 // Se una è più corta dell'altra allora la stampiamo davanti
 // Se hanno stessa lunghezza le stampiamo nell'ordine che c'è stato dato
