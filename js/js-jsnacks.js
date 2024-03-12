b10dababf8893a21158d0c6e1cb5653551c0c36b
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 14:23:02 +0100

    feat: add the else condition for odd number and print the next number which is alway an even number using the ++

--- js/js-jsnacks.js
+++ js/js-jsnacks.js
@@ -1,9 +1,12 @@
 // L'utente inserisce un numero nel prompt, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
 
-const userNumber = parseInt(prompt('Scrivi un numero')); // Chiediamo un numero 
+let userNumber = parseInt(prompt('Scrivi un numero')); // Chiediamo un numero 
 
 if (userNumber % 2 === 0) { // Verifichiamo se il numero in questione è pari
     console.log(userNumber) // Se è pari stampiamo il numero in console
-} 
-
+} else {
+    userNumber++; // Se non è pari aggiungiamo + 1 per renderlo pari 
+    console.log(userNumber) // Stampiamo il numero con + 1 in console
+}
+ 
