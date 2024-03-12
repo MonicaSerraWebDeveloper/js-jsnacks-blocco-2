07df42291b22946249ec660a5f4c4fae6ac4d29a
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 15:06:37 +0100

    feat: add two div with two different class to specify the different colors

--- js/jssnack-3/script.js
+++ js/jssnack-3/script.js
@@ -2,6 +2,8 @@
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
+
+
 // Creiamo due div in HTML e inseriamo un testo per verifica
 // Inseriamo al primo div una classe che dà colore rosso al testo
 // Inseriamo al secondo div una classe che dà colore verde al testo
@@ -9,4 +11,3 @@
 // Verifichiamo quale scorrendo l'intera lista quale numero è pari e quale è dispari
 // Scriviamo i numeri dispari e li inseriamo nel div con il testo rosso
 // Scriviamo i numeri pari nel div con il testo verde
-
