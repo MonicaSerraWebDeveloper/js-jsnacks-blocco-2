9503c9a7132fed2c5c74c4c36b4c51cebd9bfe37
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 14:30:56 +0100

    feat: add the two variables to ask a word to the user

--- js/jssnack-2/script.js
+++ js/jssnack-2/script.js
@@ -1,4 +1,8 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
-
+const firstWord = prompt('Scrivi una parola'); // Chiediamo due parole con due prompt
+const secondWord = prompt("Scrivi un'altra parola");
+// Verifichiamo se una è più corta dell'altra
+// Se una è più corta dell'altra allora la stampiamo davanti
+// Se hanno stessa lunghezza le stampiamo nell'ordine che c'è stato dato
