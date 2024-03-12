faf3c0d4461392346029e6b6116ae471d1050fba
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 16:32:28 +0100

    fix: deleted the variable let outside the for cycle

--- js/jssnack-3/script.js
+++ js/jssnack-3/script.js
@@ -8,23 +8,23 @@
 // Inseriamo al secondo div una classe che dà colore verde al testo
 
 const arrayLength = 20; // Stabiliamo un numero massimo di numeri casuali
-const listOfNumbers = []; // Scriviamo un array con una lista di numeri casuali
+const listOfNumbers = [21, 4, 5, 7, 98, 89, 6, 32]; // Scriviamo un array con una lista di numeri casuali
 
 for (let x = 0; x < arrayLength; x++) { // Scriviamo il ciclo di numeri casuali
     let randomNumberList = Math.floor(Math.random() * 100);
     listOfNumbers.push(randomNumberList);
 }
 
-let allTheNumbers = 0;
-
 for (let i = 0; i < listOfNumbers.length; i++) {
-    allTheNumbers = listOfNumbers[i];
-    
+    const evenAndOdd = listOfNumbers[i];
+
     const addList = document.createElement('li');
-    addList.innerHTML = allTheNumbers;
-    if (allTheNumbers % 2 === 0) { // Verifichiamo quale numero, scorrendo l'intera lista, è pari e quale è dispari
-        document.querySelector('.even-number').append(addList); // Scriviamo i numeri pari nel div con il testo verde
+    addList.innerHTML = evenAndOdd;
+    if (evenAndOdd % 2 === 0) { // Verifichiamo quale numero, scorrendo l'intera lista, è pari e quale è dispari
+        document.querySelector('#even-number').append(addList);
+        // Scriviamo i numeri pari nel div con il testo verde
     } else {
-        document.querySelector('.odd-number').append(addList); // Scriviamo i numeri dispari e li inseriamo nel div con il testo rosso
+        document.querySelector('#odd-number').append(addList);
+         // Scriviamo i numeri dispari e li inseriamo nel div con il testo rosso
     }
 }
