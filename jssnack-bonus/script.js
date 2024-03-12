7c0306cb24e9c7aaeacea7efdc6304e2889c5745
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 16:47:18 +0100

    feat: add all the comment to understand better the process

--- jssnack-bonus/script.js
+++ jssnack-bonus/script.js
@@ -1,3 +1,5 @@
 // Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
 
-// 
+// Chiediamo un numero di 4 cifre 
+// Prendiamo in considerazione le singole cifre
+// Sommiamo le cifre l'una con l'altra
