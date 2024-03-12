8e02246384691a737e2393dde33401131b459f3e
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 15:01:07 +0100

    feat: add all the comments to fragment the process

---
+++ js/jssnack-3/script.js
@@ -0,0 +1,12 @@
+// Crea due tag div con due id diversi:
+// un div avrà il testo colorato di rosso mentre l’altro di verde.
+// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+// Creiamo due div in HTML e inseriamo un testo per verifica
+// Inseriamo al primo div una classe che dà colore rosso al testo
+// Inseriamo al secondo div una classe che dà colore verde al testo
+// Scriviamo un array con una lista di numeri casuali
+// Verifichiamo quale scorrendo l'intera lista quale numero è pari e quale è dispari
+// Scriviamo i numeri dispari e li inseriamo nel div con il testo rosso
+// Scriviamo i numeri pari nel div con il testo verde
+
