a87ef6c988e226b2cfa6bded5556354043cad175
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 16:51:01 +0100

    feat: add the variable to define the prompt

--- jssnack-bonus/script.js
+++ jssnack-bonus/script.js
@@ -1,5 +1,8 @@
 // Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
 
-// Chiediamo un numero di 4 cifre 
+const fourFiguresNumber = parseInt(prompt('Scrivi un numero di 4 cifre')); // Chiediamo un numero di 4 cifre 
+console.log(fourFiguresNumber);
+
+
 // Prendiamo in considerazione le singole cifre
 // Sommiamo le cifre l'una con l'altra
