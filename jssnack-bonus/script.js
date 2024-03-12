f96afe7399cfe41b082fd2892109399d8b0fceb2
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 17:03:17 +0100

    feat: add the variable outside the for cycle to do the sum with every figure inside the prompt and print it in the console log

--- jssnack-bonus/script.js
+++ jssnack-bonus/script.js
@@ -2,10 +2,15 @@
 
 const fourFiguresNumber = prompt('Scrivi un numero di 4 cifre'); // Chiediamo un numero di 4 cifre 
 
+let sum = 0;
+
 for (let i = 0; i < fourFiguresNumber.length; i++) {
     const stringToNumber = parseInt(fourFiguresNumber[i]);
     console.log(stringToNumber);
-
+    sum += stringToNumber;
 }
+
+console.log(sum);
+
 // Prendiamo in considerazione le singole cifre
 // Sommiamo le cifre l'una con l'altra
