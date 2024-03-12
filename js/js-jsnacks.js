e2fdb576068dad15775b7fd374380f0262ecc46d
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 14:20:01 +0100

    feat: add the condition in case the number is even we print it in the console

--- js/js-jsnacks.js
+++ js/js-jsnacks.js
@@ -3,4 +3,7 @@
 
 const userNumber = parseInt(prompt('Scrivi un numero')); // Chiediamo un numero 
 
+if (userNumber % 2 === 0) { // Verifichiamo se il numero in questione è pari
+    console.log(userNumber) // Se è pari stampiamo il numero in console
+} 
 
