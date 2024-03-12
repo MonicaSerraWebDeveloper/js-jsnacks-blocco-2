9b9c538c814b042bbed56733c361e0d3e27b6d26
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 14:19:19 +0100

    feat: add a variable to ask a number to the user

--- js/js-jsnacks.js
+++ js/js-jsnacks.js
@@ -1,3 +1,6 @@
 // L'utente inserisce un numero nel prompt, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
 
+const userNumber = parseInt(prompt('Scrivi un numero')); // Chiediamo un numero 
+
+
